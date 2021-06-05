0bf330a46cf283e93d4d2e9f37660c3eab446878
Jim Coffey <telic@hotmail.co.uk>
2021-06-05 15:27:11 +0100

    c1

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -18,6 +18,7 @@ const mapStateToProps = (): IStateToProps => ({});
 const mapDispatchToProps = (dispatch: Dispatch<IAction<any>>): IDispatchToProps => ({
   dispatch
 });
+const myVar = 1;
 
 class App extends React.Component<IStateToProps & IDispatchToProps & IProps, IState> {
   public onClickButtonHandler = this._onClickButton.bind(this);
