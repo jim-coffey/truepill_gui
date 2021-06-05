212f0fea86880ab77befca2e1710f591ae5fb10d
Jim Coffey <telic@hotmail.co.uk>
2021-06-05 15:33:39 +0100

    c3

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -18,6 +18,7 @@ const mapStateToProps = (): IStateToProps => ({});
 const mapDispatchToProps = (dispatch: Dispatch<IAction<any>>): IDispatchToProps => ({
   dispatch
 });
+const myVar3 = 3;
 const myVar = 1;
 const myVar2 = 2;
 
